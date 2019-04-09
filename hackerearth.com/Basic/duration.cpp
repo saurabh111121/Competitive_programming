b6b22b841141cd6473ad22bad008086d6256dfb1
Saurabh kala <37699322+saurabh111121@users.noreply.github.com>
2019-04-09 22:23:31 +0530

    Update duration.cpp

--- hackerearth.com/Basic/duration.cpp
+++ hackerearth.com/Basic/duration.cpp
@@ -1,3 +1,4 @@
+//https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/duration/
 #include<iostream>
 using namespace std;
 int abs(int c){
