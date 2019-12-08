e32b7b416ade5297020f6a940b592e721edb173b
Saurabh kala <37699322+saurabh111121@users.noreply.github.com>
2019-12-08 22:09:27 +0530

    Create binary_queues.cpp
    https://www.hackerearth.com/practice/data-structures/arrays/1-d/practice-problems/algorithm/range-query-2/

---
+++ hackerearth.com/Basic/binary_queues.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+using namespace std;
+int main(){
+ int n,q,l,r;
+ cin >> n >> q;
+ int a[n];
+ for(int i=0;i<n;i++){
+    cin >> a[i];
+  }
+  while(q>0){
+    cin >> 
+    q--;
+  }
+
+}
