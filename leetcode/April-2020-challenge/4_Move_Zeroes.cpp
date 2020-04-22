78eeea5a1b0cecb5bd39b87bb7724fbce1f536ad
Saurabh kala <37699322+saurabh111121@users.noreply.github.com>
2020-04-22 18:53:55 +0530

    Update 4_Move_Zeroes.cpp

--- leetcode/April-2020-challenge/4_Move_Zeroes.cpp
+++ leetcode/April-2020-challenge/4_Move_Zeroes.cpp
@@ -1,1 +1,29 @@
+/*
+Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 
+Example:
+
+Input: [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Note:
+
+You must do this in-place without making a copy of the array.
+Minimize the total number of operations.*/
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int nxt = 0;
+        for(int x : nums) {
+            if(x != 0) {
+                nums[nxt] = x;
+                nxt++;
+            }
+        }
+        for(int i = nxt; i < n; i++) {
+            nums[i] = 0;
+        }   
+        //return nums;
+    }
+};
+© 2020 GitHub, In
